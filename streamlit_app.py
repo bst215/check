b84c75679c202d0e456572146c987ffcb093770f
Ronald Coe <104688933+sfc-gh-rcoe@users.noreply.github.com>
2022-11-26 13:42:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ t_file = st.sidebar.file_uploader("Pick a PDF File")
 
 if (t_file != None):
    t_contents = PdfReader(t_file)
-   num_pages = len(t_contents.ages)
+   num_pages = len(t_contents.pages)
    with m_cont:
       st.write("Number of pages in the .PDF: " + str(num_pages))
