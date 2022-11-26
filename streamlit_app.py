5818befdd8a7c65d61dd22e3857ebd3c74814247
Ronald Coe <104688933+sfc-gh-rcoe@users.noreply.github.com>
2022-11-26 16:26:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,6 @@ if (t_file != None):
    # num_pages = len(t_contents.pages)
    for page in img:
       pic = page.get_pixmap()
-      st.image(pic, output_format='PNG')
+      st.image(pic.getImageData("PNG"), output_format='PNG')
 
 
