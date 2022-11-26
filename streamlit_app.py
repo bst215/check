4c2009fb2c6c4bdbb37e21748a417a1ebac5fa28
Ronald Coe <104688933+sfc-gh-rcoe@users.noreply.github.com>
2022-11-26 15:20:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import cv2
 from PyPDF2 import PdfReader, PdfWriter
 from PIL import Image
 from datetime import datetime
-from pdf2image import convert_from_bytes
+from pdf2image import convert_from_bytes, convert_from_path
 import io
 import os
 
@@ -16,26 +16,16 @@ t_file = st.sidebar.file_uploader("Pick a PDF File")
 
 if (t_file != None):
    dst_pdf = PdfWriter()
-   t_contents = PdfReader(t_file)
-   num_pages = len(t_contents.pages)
+   # t_contents = PdfReader(t_file)
+   img = convert_from_path(t_file)
+   # num_pages = len(t_contents.pages)
+   num_pages = len(img)
    if (num_pages > 0):
-      page_one = t_contents.pages[0]
-      page_one_text = page_one.extract_text()
+      page_one = img[0]
+      # page_one_text = page_one.extract_text()
       with m_cont:
          st.write("Number of pages in the .PDF: " + str(num_pages))
-         for i in range(num_pages):
-            curr_page_content = t_contents.pages[i]
-            curr_page_txt = curr_page_content.extract_text()
-            st.write(curr_page_txt)
-            dst_pdf.addPage(t_contents.getPage(i))
-      st.write(t_contents.pages)
-      pdf_bytes = io.BytesIO()
-      dst_pdf.write(pdf_bytes)
-      pdf_bytes.seek(0)
-      img = convert_from_bytes(pdf_bytes)
-      st.image(img)
-      # t_fileName = now.strftime("%H%M%S") + ".jpg"
-      # img.save(t_fileName, 'JPG')
-      # pdf_bytes.flush()
-      #st.write(t_fileName)
+      for k in range(num_pages):
+         st.image(img[k])
+
 
