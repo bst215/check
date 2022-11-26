62b84ca90700e4756f015bdfbb671ce59ebcc33c
Ronald Coe <104688933+sfc-gh-rcoe@users.noreply.github.com>
2022-11-26 16:08:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from pdf2image import convert_from_bytes, convert_from_path
 import io
 import os
+import fitz
 
 st.write("Welcome!")
 
@@ -17,15 +18,10 @@ t_file = st.sidebar.file_uploader("Pick a PDF File")
 if (t_file != None):
    dst_pdf = PdfWriter()
    # t_contents = PdfReader(t_file)
-   img = convert_from_bytes(t_file.getvalue())
+   img = fitz.open(t_file.name)
    # num_pages = len(t_contents.pages)
-   num_pages = len(img)
-   if (num_pages > 0):
-      page_one = img[0]
-      # page_one_text = page_one.extract_text()
-      with m_cont:
-         st.write("Number of pages in the .PDF: " + str(num_pages))
-      for k in range(num_pages):
-         st.image(img[k])
+   for page in img:
+      pic = page.get_pixmap(matrix=mat)
+      st.image(pic)
 
 
