1bd25ced0191bd67dd1207a325df61db5883a7a5
Ronald Coe <104688933+sfc-gh-rcoe@users.noreply.github.com>
2022-11-26 23:00:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if (t_file != None):
             t_read = reader.readtext(pic.pil_tobytes(format="JPEG"))
             for t in range(len(t_read)):
                # m_dict.update({t: reader.readtext(pic.pil_tobytes(format="JPEG"))[t][1]})
-               m_dict.update({g: t_read[g][1]})
+               m_dict.update({g: t_read[t][1]})
                g+=1
       with c2:
         st.write(m_dict)
