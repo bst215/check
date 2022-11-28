1587e65e11a072c7d5a6e7bc88875be6ad734e6f
ronaldcoe-snow <ronald.coe@snowflake.com>
2022-11-28 15:10:56 +0000

    work on page two

--- pages/1_Marked_Up_Image(s).py
+++ pages/1_Marked_Up_Image(s).py
@@ -9,4 +9,10 @@ import easyocr
 import numpy as np
 import pandas as pd
 
+st.set_page_config(layout="wide")
 st.write("Page Two")
+
+
+
+    for i in range(len(c_pages)):
+        st.image(c_pages[i])
